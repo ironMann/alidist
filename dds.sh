73c7449694d47b83b2370b52f645d5cacb9033a9
Kostas Alexopoulos <kostorr.alex@gmail.com>
2021-03-17 16:19:29 +0200

    Fix libboost* deletion from INSTALLROOT for DDS (#2905)

--- dds.sh
+++ dds.sh
@@ -39,7 +39,7 @@ esac
 
 make -j$JOBS install
 
-find $INSTALLROOT -path "*/lib/libboost_*" -o -path "*/lib64/libboost_*" -delete
+find $INSTALLROOT -path "*/lib/libboost_*" -delete
 rm -f "$INSTALLROOT/LICENSE"
 
 # ModuleFile
